ed40d0014aaff7dff72a0a90cd9a8223925c38f4
JonatanRamos919 <ramosjona919@gmail.com>
2025-04-25 09:31:09 -0600

    commit inicial

---
+++ planilla de empleado/src/planilla/de/empleado/PlanillaDeEmpleado.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package planilla.de.empleado;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jona Ramos
+ */
+public class PlanillaDeEmpleado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try ( // TODO code application logic here
+                Scanner scanner = new Scanner(System.in)) {
+            String nombreEmpleado = scanner.next("Jonatan Ramos");
+            int horasMensuales = scanner.nextInt(160) ;
+            int tarifaHora = scanner.nextInt(500);
+            int salarioSemanal = scanner.nextInt(20000);
+            
+            System.out.println("\n****Boleta del empleado****");
+            System.out.println("Hora de Trabajo Mensual :" + nombreEmpleado);
+            System.out.println("Hora de Trabajo Mensual :" + horasMensuales);
+            System.out.println("Tarifa por Hora :Lps." + tarifaHora);
+            System.out.println("Salario del Empleado Semanal :lps." + salarioSemanal);
+        }
+    
+}
+    
+}
