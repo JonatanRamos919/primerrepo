70f1762afd2e01f8861d191bda1fbde6394b45cb
JonatanRamos919 <ramosjona919@gmail.com>
2025-04-25 11:48:11 -0600

    clics cambio

---
+++ planilla de empleado/src/semana1/clics.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package semana1;
+
+/**
+ *
+ * @author Jona Ramos
+ */
+public class clics {
+    public static void main (String[] arg){
+        
+        int clic1 = 60;
+        double costo1 = 0.30;
+        int clic2 = 100;
+        double costo2 = 0.25;
+        int clic3 = 20;
+        double costo3 = 0.80;
+        
+       double total = (clic1*costo1) + (clic2*costo2) + (clic3*costo3);
+       
+       
+       
+               
+     
+        
+        
+    }
+    
+}
